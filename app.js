13cd26aa12f24e770d83c4b9ef78c6e3fe9edfe8
agha_rameez <agha.rameez@hypernymbiz.com>
2021-05-29 15:31:24 +0500

    deployement settings

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var cool = require('cool-ascii-faces');
 ///Importing modules/ packages ///
 
 ///defining port ///
-const port = 3000
+const port = process.env.PORT || 3000
 ///defining port ///
 var settings = require('./config/config')
 console.log(settings)
